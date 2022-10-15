6d080a7fd7b4698b895303b5d988d0e06c7cec1a
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-15 22:01:34 +0500

    commit1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Comparison operator
+![](./img/js1.jpg)
+#
+# Comparison logical operator
+#
+## Operator and/&& 
+![](./img/js2.jpg)
+#
+## Operator or/||
+![](./img/js3.jpg)
+#
+## Operator not/!
+![](./img/js4.jpg)
---
+++ main.js
@@ -0,0 +1,30 @@
+// // Comparison operator
+
+// console.log(5==5)    // true
+// console.log(5!=5)    // false
+// console.log(5==="5") // false
+// console.log(5!=="5") // true
+// console.log(5>2)     // true
+// console.log(5>=5)    // true
+// console.log(5<10)    // true
+// console.log(5<=5)    // true
+
+// // logical operator
+
+// // and &&
+// console.log(true && true);   // true
+// console.log(true && false);  // false
+// console.log(false && true); // false
+// console.log(false && false); // false
+
+// // or ||
+// console.log(true || true);   // true
+// console.log(true || false);  // true
+// console.log(false || true);  // true
+// console.log(false || false); // false
+
+// // not !
+// console.log(!true);   // false
+// console.log(!false);  // true
+
+// if / else
