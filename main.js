099b438ff0dee2e85abd8ead093d8b12f1ef9363
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-15 23:12:42 +0500

    commit5

--- main.js
+++ main.js
@@ -65,20 +65,60 @@
 // }
 // // 0
 
-// switch
+// // switch
 
-let a = 8;
+// let a = 8;
 
-for(let i=0; i<a; i++){
-    if(a < 10 && a>5){
-        break;
-        a = a*2;
-    }
+// for(let i=0; i<a; i++){
+//     if(a < 10 && a>5){
+//         break;
+//         a = a*2;
+//     }
+// }
+// console.log(a);
+
+// // ternary
+
+// let b = "chris" ? 100:999;
+// console.log(b); // 100
+
+
+// // for
+
+// let a = 10;
+// let b = 10;
+
+// for(let i=1; i<=b; i++){
+//     a = a + a;
+// }
+// console.log(a); // 10240
+
+// // while
+
+// let num = 5;
+// let num2 = 3
+// let i = 1;
+// while(i <= num){
+//     console.log(num2);
+//     i++;
+// }
+// // 3 3 3 3 3
+
+// // break
+
+// let num = 5;
+// let i = 1;
+// while(i <= num){
+//     if(i == 3){ break;}
+//     console.log(i);
+//     i++;
+// }
+// // 1 2 
+
+for(let i=0; i<10; i++){
+    if(i % 2 != 0){continue;}
+
+    console.log(i);
 }
-console.log(a);
-
-// ternary
-
-let b = "chris" ? 100:999;
-console.log(b); // 100
+// 0 2 4 6 8
 
